aaaba35d5cdbb399f925bb7a42cc68d844b60fac
Marcin <77637725+Dragoon497@users.noreply.github.com>
2026-08-23 20:05:11 +0200

    Add files via upload

---
+++ images/presets.js
@@ -0,0 +1,307 @@
+window.PRESET_BACKGROUNDS=[];
+window.AVATAR_PRESETS=[
+  {
+    "src": "images/Aether_Icon.png",
+    "label": "Aether Icon"
+  },
+  {
+    "src": "images/Albedo_Icon.png",
+    "label": "Albedo Icon"
+  },
+  {
+    "src": "images/Alhaitham_Icon.png",
+    "label": "Alhaitham Icon"
+  },
+  {
+    "src": "images/Aloy_Icon.png",
+    "label": "Aloy Icon"
+  },
+  {
+    "src": "images/Amber_Icon.png",
+    "label": "Amber Icon"
+  },
+  {
+    "src": "images/Arataki_Itto_Icon.png",
+    "label": "Arataki Itto Icon"
+  },
+  {
+    "src": "images/Baizhu_Icon.png",
+    "label": "Baizhu Icon"
+  },
+  {
+    "src": "images/Barbara_Icon.png",
+    "label": "Barbara Icon"
+  },
+  {
+    "src": "images/Beidou_Icon.png",
+    "label": "Beidou Icon"
+  },
+  {
+    "src": "images/Bennett_Icon.png",
+    "label": "Bennett Icon"
+  },
+  {
+    "src": "images/Candace_Icon.png",
+    "label": "Candace Icon"
+  },
+  {
+    "src": "images/Chongyun_Icon.png",
+    "label": "Chongyun Icon"
+  },
+  {
+    "src": "images/Collei_Icon.png",
+    "label": "Collei Icon"
+  },
+  {
+    "src": "images/Cyno_Icon.png",
+    "label": "Cyno Icon"
+  },
+  {
+    "src": "images/Dehya_Icon.png",
+    "label": "Dehya Icon"
+  },
+  {
+    "src": "images/Diluc_Icon.png",
+    "label": "Diluc Icon"
+  },
+  {
+    "src": "images/Diona_Icon.png",
+    "label": "Diona Icon"
+  },
+  {
+    "src": "images/Dori_Icon.png",
+    "label": "Dori Icon"
+  },
+  {
+    "src": "images/Eula_Icon.png",
+    "label": "Eula Icon"
+  },
+  {
+    "src": "images/Faruzan_Icon.png",
+    "label": "Faruzan Icon"
+  },
+  {
+    "src": "images/Fischl_Icon.png",
+    "label": "Fischl Icon"
+  },
+  {
+    "src": "images/Freminet_Icon.png",
+    "label": "Freminet Icon"
+  },
+  {
+    "src": "images/Ganyu_Icon.png",
+    "label": "Ganyu Icon"
+  },
+  {
+    "src": "images/Gorou_Icon.png",
+    "label": "Gorou Icon"
+  },
+  {
+    "src": "images/Hu_Tao_Icon.png",
+    "label": "Hu Tao Icon"
+  },
+  {
+    "src": "images/Jean_Icon.png",
+    "label": "Jean Icon"
+  },
+  {
+    "src": "images/Kaedehara_Kazuha_Icon.png",
+    "label": "Kaedehara Kazuha Icon"
+  },
+  {
+    "src": "images/Kaeya_Icon.png",
+    "label": "Kaeya Icon"
+  },
+  {
+    "src": "images/Kamisato_Ayaka_Icon.png",
+    "label": "Kamisato Ayaka Icon"
+  },
+  {
+    "src": "images/Kamisato_Ayato_Icon.png",
+    "label": "Kamisato Ayato Icon"
+  },
+  {
+    "src": "images/Kaveh_Icon.png",
+    "label": "Kaveh Icon"
+  },
+  {
+    "src": "images/Keqing_Icon.png",
+    "label": "Keqing Icon"
+  },
+  {
+    "src": "images/Kirara_Icon.png",
+    "label": "Kirara Icon"
+  },
+  {
+    "src": "images/Klee_Icon.png",
+    "label": "Klee Icon"
+  },
+  {
+    "src": "images/Kujou_Sara_Icon.png",
+    "label": "Kujou Sara Icon"
+  },
+  {
+    "src": "images/Kuki_Shinobu_Icon.png",
+    "label": "Kuki Shinobu Icon"
+  },
+  {
+    "src": "images/Layla_Icon.png",
+    "label": "Layla Icon"
+  },
+  {
+    "src": "images/Lisa_Icon.png",
+    "label": "Lisa Icon"
+  },
+  {
+    "src": "images/Lumine_Icon.png",
+    "label": "Lumine Icon"
+  },
+  {
+    "src": "images/Lynette_Icon.png",
+    "label": "Lynette Icon"
+  },
+  {
+    "src": "images/Lyney_Icon.png",
+    "label": "Lyney Icon"
+  },
+  {
+    "src": "images/Mika_Icon.png",
+    "label": "Mika Icon"
+  },
+  {
+    "src": "images/Mona_Icon.png",
+    "label": "Mona Icon"
+  },
+  {
+    "src": "images/Nahida_Icon.png",
+    "label": "Nahida Icon"
+  },
+  {
+    "src": "images/Neuvillette_Icon.png",
+    "label": "Neuvillette Icon"
+  },
+  {
+    "src": "images/Nilou_Icon.png",
+    "label": "Nilou Icon"
+  },
+  {
+    "src": "images/Ningguang_Icon.png",
+    "label": "Ningguang Icon"
+  },
+  {
+    "src": "images/Noelle_Icon.png",
+    "label": "Noelle Icon"
+  },
+  {
+    "src": "images/Paimon_Icon.png",
+    "label": "Paimon Icon"
+  },
+  {
+    "src": "images/Qiqi_Icon.png",
+    "label": "Qiqi Icon"
+  },
+  {
+    "src": "images/Raiden_Shogun_Icon.png",
+    "label": "Raiden Shogun Icon"
+  },
+  {
+    "src": "images/Razor_Icon.png",
+    "label": "Razor Icon"
+  },
+  {
+    "src": "images/Rosaria_Icon.png",
+    "label": "Rosaria Icon"
+  },
+  {
+    "src": "images/Sangonomiya_Kokomi_Icon.png",
+    "label": "Sangonomiya Kokomi Icon"
+  },
+  {
+    "src": "images/Sayu_Icon.png",
+    "label": "Sayu Icon"
+  },
+  {
+    "src": "images/Shenhe_Icon.png",
+    "label": "Shenhe Icon"
+  },
+  {
+    "src": "images/Shikanoin_Heizou_Icon.png",
+    "label": "Shikanoin Heizou Icon"
+  },
+  {
+    "src": "images/Sucrose_Icon.png",
+    "label": "Sucrose Icon"
+  },
+  {
+    "src": "images/Tartaglia_Icon.png",
+    "label": "Tartaglia Icon"
+  },
+  {
+    "src": "images/Thoma_Icon.png",
+    "label": "Thoma Icon"
+  },
+  {
+    "src": "images/Tighnari_Icon.png",
+    "label": "Tighnari Icon"
+  },
+  {
+    "src": "images/Traveler_Icon.png",
+    "label": "Traveler Icon"
+  },
+  {
+    "src": "images/Venti_Icon.png",
+    "label": "Venti Icon"
+  },
+  {
+    "src": "images/Wanderer_Icon.png",
+    "label": "Wanderer Icon"
+  },
+  {
+    "src": "images/Wriothesley_Icon.png",
+    "label": "Wriothesley Icon"
+  },
+  {
+    "src": "images/Xiangling_Icon.png",
+    "label": "Xiangling Icon"
+  },
+  {
+    "src": "images/Xiao_Icon.png",
+    "label": "Xiao Icon"
+  },
+  {
+    "src": "images/Xingqiu_Icon.png",
+    "label": "Xingqiu Icon"
+  },
+  {
+    "src": "images/Xinyan_Icon.png",
+    "label": "Xinyan Icon"
+  },
+  {
+    "src": "images/Yae_Miko_Icon.png",
+    "label": "Yae Miko Icon"
+  },
+  {
+    "src": "images/Yanfei_Icon.png",
+    "label": "Yanfei Icon"
+  },
+  {
+    "src": "images/Yaoyao_Icon.png",
+    "label": "Yaoyao Icon"
+  },
+  {
+    "src": "images/Yelan_Icon.png",
+    "label": "Yelan Icon"
+  },
+  {
+    "src": "images/Yoimiya_Icon.png",
+    "label": "Yoimiya Icon"
+  },
+  {
+    "src": "images/Yun_Jin_Icon.png",
+    "label": "Yun Jin Icon"
+  },
+  {
+    "src": "images/Zhongli_Icon.png",
+    "label": "Zhongli Icon"
+  }
+];
